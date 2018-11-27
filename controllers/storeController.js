3749565fe9d47c16b736e78df159d383ba40eda7
Harun R Rayhan <harun.b13@gmail.com>
2018-11-27 19:38:54 +0600

    Flash Message and Reditect to Store after creation

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -10,7 +10,11 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  res.redirect("/");
+  // prettier-ignore
+  const store = await (new Store(req.body)).save();
+  req.flash(
+    "success",
+    `Successfully create ${store.name}!. Care to leave a review?`
+  );
+  res.redirect(`/store/${store.slug}`);
 };
