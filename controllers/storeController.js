f61b5143505da7626dad752d405babb17bae83c6
Harun R Rayhan <harun.b13@gmail.com>
2018-11-28 15:49:12 +0600

    Edit Store Form

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -24,3 +24,28 @@ exports.getStores = async (req, res) => {
   const allStores = await Store.find();
   res.render("stores", { title: "Stores", stores: allStores });
 };
+
+exports.editStore = async (req, res) => {
+  // 1. Find The store by given ID
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. confirm they are the owner of the store
+  // TODO
+  // 3. Render the edit form
+  res.render("editStore", { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  // 1. Find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true
+  }).exec();
+  // 2. Redirect them to the store and show flash message
+  req.flash(
+    "success",
+    `Successfully Updated <strong>${store.name}</strong>. <a href="/store/${
+      store.slug
+    }">View Store -></a>`
+  );
+  res.redirect(`/stores/${store._id}/edit`);
+};
