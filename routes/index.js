a26413d1b37ab228b819be5cfbefc6f307960932
Harun R Rayhan <harun.b13@gmail.com>
2018-12-01 16:59:06 +0600

    Show user heated stores

--- routes/index.js
+++ routes/index.js
@@ -58,6 +58,11 @@ router.post(
   catchErrors(authController.update)
 );
 router.get("/map", storeController.mapPage);
+router.get(
+  "/hearts",
+  authController.isLoggedIn,
+  catchErrors(storeController.getHearts)
+);
 
 /*
   API
